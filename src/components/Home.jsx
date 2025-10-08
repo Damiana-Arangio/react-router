37bfa20e62b432cf31403730554b0156947af7bb
Damiana-Arangio <arangio.damiana@gmail.com>
2025-10-08 20:06:52 +0200

    Ottimizzazione Navbar e componente Home: - Gestita classe "active" - Migliorati stile e layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,43 @@
 function Home() {
     return(
 
-        <div className="container">
-            {/* Testo Promozionale */}
-                <h1> Benvenuto nel nostro mini e-commerce</h1>
-                <p> Approfitta delle nostre offerte speciali! </p>
-            
+<<<<<<< HEAD
+        <div className="container-page">
+           
+            <h1> Fake Store API </h1>
+            <div className="container-flex-left-right">
+
                 {/* Immagine Promozionale */}
-                <figure>
-                    <img className="img-promozionale" src="./src/assets/img/marketing-festività.png" alt="immagine marketing" />
-                </figure>
+                <div className="container-left">
+                    <figure>
+                        <img src="./src/assets/img/marketing-festività.png" alt="immagine marketing" />
+=======
+        <div className="container">
+           
+            <h1> Fake Store API </h1>
+            <div className="hero">
+
+                {/* Immagine Promozionale */}
+                <div className="container-img-promozionale">
+                    <figure>
+                        <img className="img-promozionale" src="./src/assets/img/marketing-festività.png" alt="immagine marketing" />
+>>>>>>> 379b29b139a20e70d06376b5787cb7f51768acc1
+                    </figure>
+                </div>
+
+                {/* Testo Promozionale */}
+<<<<<<< HEAD
+                <div className="container-right">
+                    <h2> Benvenuto nel nostro mini e-commerce</h2>
+                    <p className="text-promozionale"> Approfitta delle nostre offerte speciali! </p>
+=======
+                <div className="container-testo-promozionale">
+                    <h2> Benvenuto nel nostro mini e-commerce</h2>
+                    <p> Approfitta delle nostre offerte speciali! </p>
+>>>>>>> 379b29b139a20e70d06376b5787cb7f51768acc1
+                </div>
+            </div>
+            
         </div>
     )
 }
