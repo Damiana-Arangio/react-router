1d9e9a2149681973d285e9f87dea653d437ce8e5
Damiana-Arangio <arangio.damiana@gmail.com>
2025-10-08 20:29:49 +0200

    Implementato componente "ChiSiamo": - Aggiunti immagine e testo - Sistemati stile e layout

--- src/components/ChiSiamo.jsx
+++ src/components/ChiSiamo.jsx
@@ -3,7 +3,26 @@
 
 function ChiSiamo() {
     return (
-        <div> Componente Chi Siamo </div>
+        <div className="container-page">
+
+            <h1> Fake Store API </h1>
+            <div className="container-flex-left-right">
+
+                {/* Immagine Punto Interrogativo */}
+                <div className="container-left">
+                    <figure>
+                        <img className="img-question" src="./src/assets/img/orange-question.png" alt="immagine marketing" />
+                    </figure>
+                </div>
+
+                {/* Testo Promozionale */}
+                <div className="container-right">
+                    <h2> Chi Siamo </h2>
+                    <p className="text-promozionale"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sequi aliquam distinctio accusamus. Totam itaque cum neque temporibus repudiandae debitis eveniet officiis rerum veritatis porro saepe, quisquam tempora eligendi quidem?</p>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@
 function Home() {
     return(
 
-<<<<<<< HEAD
         <div className="container-page">
            
             <h1> Fake Store API </h1>
@@ -13,31 +12,14 @@ function Home() {
                 {/* Immagine Promozionale */}
                 <div className="container-left">
                     <figure>
-                        <img src="./src/assets/img/marketing-festività.png" alt="immagine marketing" />
-=======
-        <div className="container">
-           
-            <h1> Fake Store API </h1>
-            <div className="hero">
-
-                {/* Immagine Promozionale */}
-                <div className="container-img-promozionale">
-                    <figure>
                         <img className="img-promozionale" src="./src/assets/img/marketing-festività.png" alt="immagine marketing" />
->>>>>>> 379b29b139a20e70d06376b5787cb7f51768acc1
                     </figure>
                 </div>
 
                 {/* Testo Promozionale */}
-<<<<<<< HEAD
                 <div className="container-right">
                     <h2> Benvenuto nel nostro mini e-commerce</h2>
                     <p className="text-promozionale"> Approfitta delle nostre offerte speciali! </p>
-=======
-                <div className="container-testo-promozionale">
-                    <h2> Benvenuto nel nostro mini e-commerce</h2>
-                    <p> Approfitta delle nostre offerte speciali! </p>
->>>>>>> 379b29b139a20e70d06376b5787cb7f51768acc1
                 </div>
             </div>
             
